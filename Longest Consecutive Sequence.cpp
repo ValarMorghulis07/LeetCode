52e436a6db9edd90d97c407dee89cd6c167e8f02
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-04-05 19:43:24 +0530

    Add files via upload

---
+++ Longest Consecutive Sequence.cpp
@@ -0,0 +1,21 @@
+int longestConsecutive(vector<int>& nums)
+    {
+     int n=nums.size();
+     if(n==0)return 0;
+     unordered_set<int>ss;
+     for(int i=0;i<n;i++)
+         ss.insert(nums[i]);
+     int ans=0;
+     for(int i=0;i<n;i++)
+     {
+      if(ss.find(nums[i]-1)==ss.end())
+      {
+       int j=nums[i];
+       while(ss.find(j)!=ss.end())
+           j++;
+       ans=max(ans,j-nums[i]);
+      }
+     }
+     return ans;
+
+    }
