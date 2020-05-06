eb96079a0c4aa9c6ec047cc2cb2916141490f177
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-05-06 19:05:36 +0530

    Add files via upload

---
+++ Next greater element of each index.cpp
@@ -0,0 +1,15 @@
+{
+     vector<int>ans(T.size(),0);
+     stack<int>stk;
+     for(int i=0;i<T.size();i++)
+     {
+      while(!stk.empty() && T[stk.top()]<T[i])
+      {
+        int j=stk.top();
+        stk.pop();
+        ans[j]=i-j;
+      }
+      stk.push(i);
+     }
+     return ans;
+    }
