2f16bad9135f4f521b246a248c8170a2d594299b
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-05-08 20:11:58 +0530

    Update 309. Best Time to Buy and Sell Stock with Cooldown.cpp

--- 309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ 309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -21,3 +21,13 @@ int maxProfit(vector<int>& prices)
       return maxx;
 
     }
+
+/*
+Define buy[i] as the max profit when you buy the stock at day i. sell[i] as the max profit when you sell the stock at day i. Therefore set buy[0] = -prices[0], that if you buy the stock at first day, the profit is -prices[0], also set sell[0] = 0, that you do nothing in the first day.
+
+sell[i]=max(buy[i-1]+prices[i], sell[i-1]-prices[i-1]+prices[i]);
+buy[i-1]+prices[i] represents buy the stock on day i-1 and sell it on day i; sell[i-1]-prices[i-1]+prices[i] represents you didn't sell the stock on day i-1 but sell it on day i (bought stock back and sell it on day i).
+
+buy[i]=max(sell[i-2]-prices[i], buy[i-1]+prices[i-1]-prices[i]);
+sell[i-2]-prices[i] means sold the stock on day i-2 and buy it on day i (day i-1 is cooldown). buy[i-1]+prices[i-1]-prices[i] means you didn't buy the stock on day i-1 but buy it on day i.
+*/
