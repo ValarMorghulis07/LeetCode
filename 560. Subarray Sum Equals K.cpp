995c52018ee1b0eb93a89504f742bee3edf20704
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-10 01:37:42 +0530

    Add files via upload

---
+++ 560. Subarray Sum Equals K.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k)
+    {
+      int n=nums.size();
+      int res=0,cur=0;
+      unordered_map<int,int>mp;
+      for(int i=0;i<n;i++)
+      {
+        cur+=nums[i];
+        if(cur==k)
+            res++;
+        if(mp.find(cur-k)!=mp.end())
+            res+=mp[cur-k];
+        mp[cur]++;
+      }
+      return res;
+    }
+};
