af7f12adc25c9c25e83db86f358f0cd2f269da59
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-10 19:25:58 +0530

    Update 347. Top K Frequent Elements.cpp

--- 347. Top K Frequent Elements.cpp
+++ 347. Top K Frequent Elements.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k)
+    vector<int> topKFrequent(vector<int>& nums, int k) 
     {
       int n=nums.size();
       if(n==k)//O(n)
@@ -10,19 +10,17 @@ public:
       unordered_map<int,int>mp;
       for(auto xx:nums)
           mp[xx]++;
-      for(auto xx:mp)
-          pq.push(make_pair(xx.second,xx.first));
-     int cnt=0;
-     while(!pq.empty() && cnt<k)//O(N*log(k))
-     {
-      int zz=pq.top().second;
-      pq.pop();
-      ans.push_back(zz);
-      cnt++;
-
-     }
-
+      for(auto xx:mp)//O(n*log(n-k))
+      {
+       pq.push(make_pair(xx.second,xx.first));
+       if(pq.size()>mp.size()-k)
+       {
+         ans.push_back(pq.top().second);
+         pq.pop();
+       }
+      }
+      
     return ans;
-
+      
     }
 };
