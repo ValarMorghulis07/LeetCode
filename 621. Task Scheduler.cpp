abbc6a6cb1641d30427879f1bd31b6fbc139a2b7
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-10 15:58:37 +0530

    Add files via upload

---
+++ 621. Task Scheduler.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    int leastInterval(vector<char>& tasks, int n)
+    {
+     int zz=tasks.size();
+     if(n==0)
+         return zz;
+     if(zz==0 || zz==1)
+         return zz;
+     map<char,int>mp;
+     int maxx=-1;
+     for(int i=0;i<tasks.size();i++)
+     {
+      mp[tasks[i]]++;
+      maxx=max(maxx,mp[tasks[i]]);
+     }
+     int cnt=0;
+     for(auto xx:mp)
+     {
+      if(xx.second==maxx)
+          cnt++;
+     }
+     int ans=max(zz,(maxx-1)*(n+1)+cnt);
+     return ans;
+
+
+
+
+
+    }
+};
