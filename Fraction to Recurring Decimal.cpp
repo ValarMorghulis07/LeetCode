23f554e2351fecfca3cf8d4646dc159ad89ce3b1
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-19 15:19:27 +0530

    Update Fraction to Recurring Decimal.cpp

--- Fraction to Recurring Decimal.cpp
+++ Fraction to Recurring Decimal.cpp
@@ -1,82 +1,60 @@
-#include<bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define per(i,b,a) for (ll i=b;i>=a;i--)
-#define all(v) (v).begin(),(v).end()
-#define ff first
-#define ss second
-#define pi acosl(-1.0l)
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define fixed(n) fixed<<setprecision(n)
-using namespace std;
-typedef long long int ll;
-typedef long double ld;
-ll ESP=1e18;
-ll FCB=1e9+7;
-ll prime=119;
-ll dir[][2]={{0,1},{0,-1},{1,0},{-1,0}};
-/*
- freopen("in.txt","r",stdin);
- freopen("out.txt","w",stdout);
-*/
-map<ll,ll>mp;
-string res;
-ll rem;
-void doit(ll num,ll den)
-{
- rem=num%den;
- while(rem!=0 && mp.find(rem)==mp.end())
- {
-  mp[rem]=res.length();
-  rem*=10;
-  res+=to_string(rem/den);
-  rem=rem%den;
- }
- /*if(rem==0)// No recurring
-    return res;
- else// Recurring
-    return res;*/
-}
-
-int main()
-{
- hs;
- ll t;
- cin>>t;
- while(t--)
- {
-  res="";
-  mp.clear();
-  ll num,den;
-  cin>>num>>den;
-  ll pp=__gcd(num,den);
-  num/=pp;
-  den/=pp;
-  string ans="";
-  if(num%den==0)
-    ans+=to_string(num/den);
-  else
-  {
-  ans+=to_string(num/den);
-  ans+=".";
-  doit(num,den);
-  //cout<<zz<<"\n";
-  if(rem==0)
-    ans+=res;
-  else
-  {
-   ans+=res.substr(0,mp[rem]);
-   ans+='(';
-   ans+=res.substr(mp[rem]);
-   ans+=')';
-  }
-  }
-  cout<<ans<<"\n";
-
- }
- return 0;
-
- }
-
+class Solution {
+public:
+    map<long long,long long>mp;
+    string res;
+    long long rem;
+    
+     void doit(long long num,long long den)
+     {
+      rem=num%den;
+      while(rem!=0 && mp.find(rem)==mp.end())
+      {
+       mp[rem]=res.length();
+       rem*=10;
+       res+=to_string(rem/den);
+       rem%=den;
+      }
+       /*if(rem==0)//Recurring
+       else //Not*/
+     }
+     
+    string fractionToDecimal(long long  numerator, long long denominator) 
+    {
+      mp.clear();
+      res="";
+      if(numerator==0)
+          return "0";
+      bool sign=(numerator<0)^(denominator<0);
+     
+      numerator=abs(numerator);
+      denominator=abs(denominator);
+      long long  num=numerator,den=denominator;
+      //cout<<num<<" "<<den;
+      long long  zz=__gcd(num,den);
+      num/=zz;
+      den/=zz;
+      string ans="";
+       if(sign)
+           ans+="-";
+      if(num%den==0)
+        ans+=to_string(num/den);
+      else
+      {
+       ans+=to_string(num/den);
+       ans+=".";
+       doit(num,den);
+       if(rem==0)
+           ans+=res;
+       else
+       {
+        ans+=res.substr(0,mp[rem]);
+        ans+='(';
+        ans+=res.substr(mp[rem]);
+        ans+=')';
+       }
+        
+      }
+        return ans;
+     }
+    
+};
