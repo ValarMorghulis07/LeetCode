c2f1c5d9cd0732806639978bd30fe5b95f04b309
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-21 01:18:06 +0530

    Add files via upload

---
+++ 179. Largest Number(Using Custom Comparator).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums)
+    {
+     int n=nums.size();
+     vector<string>vv;
+     for(int i=0;i<n;i++)
+         vv.push_back(to_string(nums[i]));
+     sort(vv.begin(),vv.end(),[](string &s1,string &s2){return s1+s2>s2+s1;});
+     string ans="";
+     for(auto xx:vv)
+         ans+=xx;
+
+     while(ans[0]=='0' && ans.size()>1)
+         ans.erase(0,1);
+     return ans;
+
+
+
+    }
+};
