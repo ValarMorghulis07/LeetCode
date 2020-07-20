97e33d8d27378df3944ddbe92c67df0da6715a48
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-21 01:33:32 +0530

    Update 179. Largest Number(Using Custom Comparator).cpp

--- 179. Largest Number(Using Custom Comparator).cpp
+++ 179. Largest Number(Using Custom Comparator).cpp
@@ -8,7 +8,7 @@ public:
      for(int i=0;i<n;i++)
          vv.push_back(to_string(nums[i]));
      sort(vv.begin(),vv.end(),[](string &s1,string &s2){return s1+s2>s2+s1;});//If largest
-    // sort(vv.begin(),vv.end(),[](string &s1,string &s2){return s1+s2<s2+s1;});//If smallest
+    // sort(vv.begin(),vv.end(),[](string &s1,string &s2){return s1+s2<s2+s1;});//If smallest && change code below accordingly
      string ans="";
      for(auto xx:vv)
          ans+=xx;
