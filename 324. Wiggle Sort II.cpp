ec32c63a7481effd3a09151fb26106a3547240c8
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-11 23:45:04 +0530

    Add files via upload

---
+++ 324. Wiggle Sort II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums)
+    {
+     vector<int>vv;
+     int n=nums.size();
+     if(n<=1)
+         return;
+     sort(nums.begin(),nums.end());
+     int i=n/2-1,j=n-1;
+     while(i>=0 || j>=n/2)
+     {
+      if(i>=0)
+       vv.push_back(nums[i]);
+      if(j>=n/2)
+       vv.push_back(nums[j]);
+      i--;
+      j--;
+     }
+     if(n>1 && vv[n-2]==vv[n-1]) // [1,2,3,5,5],(vv[n-1]==vv[n-2] is basically nums[n/2]==nums[1+n/2] after sorting nums)
+     {
+      for(int i=2;i<n-1;i++)
+      {
+       if(vv[n-1]<vv[i-1] && vv[n-1]<vv[i+1] && vv[n-2]>vv[i])
+       {
+         swap(vv[i],vv[n-1]);
+         break;
+       }
+      }
+     }
+     for(int i=0;i<n;i++)
+         nums[i]=vv[i];
+
+
+    }
+};
