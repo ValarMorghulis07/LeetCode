29a949d849225267bd58270f97ceeaabcd462b7d
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-12 15:04:40 +0530

    Update Sum Problems.cpp

--- Sum Problems.cpp
+++ Sum Problems.cpp
@@ -36,6 +36,8 @@ public:
 
 // 15. 3Sum
 
+// Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
 vector<vector<int>> threeSum(vector<int>& nums)
     {
      vector<vector<int>>vec;
