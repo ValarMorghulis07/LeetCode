060daf7596bd57ddb5650fcab2bbd61c21687d5d
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-12 13:38:45 +0530

    Update 152. Maximum Product Subarray.cpp

--- 152. Maximum Product Subarray.cpp
+++ 152. Maximum Product Subarray.cpp
@@ -1,3 +1,28 @@
+// 628. Maximum Product of Three Numbers
+
+// Given an integer array, find three numbers whose product is maximum and output the maximum product.
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums)
+    {
+     int n=nums.size();
+     bool ff=0;
+     for(int i=0;i<n;i++)
+     {
+       if(nums[i]==0)
+           ff=1;
+     }
+     sort(nums.begin(),nums.end());
+     if(ff)
+        return max(0,max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]));
+     else
+         return max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
+    }
+};
+
+// 152. Maximum Product Subarray
+
 int maxProduct(vector<int>& nums)
     {
      int n=nums.size();
@@ -11,3 +36,30 @@ int maxProduct(vector<int>& nums)
      }
      return ans;
     }
+
+// 713. Subarray Product Less Than K
+
+// Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+
+int numSubarrayProductLessThanK(vector<int>& nums, int k)
+    {
+     int n=nums.size();
+     int j=0,start=0,ans=0,zz=1,len;
+     while(j<n)
+     {
+      zz*=nums[j];
+      while(start<j && zz>=k)
+      {
+       zz/=nums[start];
+       start++;
+      }
+      if(zz<k)
+      {
+       len=j-start+1;
+       ans+=len;
+      }
+      j++;
+     }
+        return ans;
+    }
+
