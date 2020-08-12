cc57c8ef10993fc28cde0d167f4a0e4db1a78753
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-12 17:23:37 +0530

    Update Duplicates,Missing,Disappeared.cpp

--- Duplicates,Missing,Disappeared.cpp
+++ Duplicates,Missing,Disappeared.cpp
@@ -165,3 +165,16 @@ int main()
  return 0;
 }
 
+// 2nd method
+for (i = 0; i < size; i++) { 
+        if (arr[abs(arr[i]) - 1] > 0) 
+            arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1]; 
+        else
+            cout << abs(arr[i]) << "\n"; 
+    } 
+  
+    cout << "and the missing element is "; 
+    for (i = 0; i < size; i++) { 
+        if (arr[i] > 0) 
+            cout << (i + 1); 
+
