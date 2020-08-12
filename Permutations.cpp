c246ad1c91fd8a78732786a0c0fe9fc69fbac693
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-12 19:50:07 +0530

    Update and rename 46. Permutations.cpp to Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,4 +1,6 @@
- vector<vector<int>>ans;
+ // 46. Permutations
+
+vector<vector<int>>ans;
     void doit(vector<int>& nums,int lo,int hi)
     {
       if(lo==hi)
@@ -21,3 +23,21 @@
       doit(nums,0,n-1);
       return ans;
     }
+
+// using STL
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+      vector<vector<int>>ans;
+      int n=nums.size();
+      if(n==0)
+          return ans;
+      sort(nums.begin(),nums.end());
+      do{
+         ans.push_back(nums);
+      }while(next_permutation(nums.begin(),nums.end()));
+      return ans;
+    }
+};
