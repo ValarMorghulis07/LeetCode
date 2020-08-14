c2f7a02df0c12da8aec9151ccaa5328ed0f6c23e
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-14 22:01:32 +0530

    Update Duplicates,Missing,Disappeared.cpp

--- Duplicates,Missing,Disappeared.cpp
+++ Duplicates,Missing,Disappeared.cpp
@@ -1,23 +1,38 @@
 // 287. Find the Duplicate Number
 
 
-int findDuplicate(vector<int>& nums)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums)
     {
-     int slow=nums[0];
-     int fast=nums[0];
-    do
-     {
-      slow=nums[slow];
-      fast=nums[nums[fast]];
+     int n=nums.size();
+     int ans;
+     /*int slow=nums[0],fast=nums[0];
+     do{
+        slow=nums[slow];
+        fast=nums[nums[fast]];
      }while(slow!=fast);
      slow=nums[0];
-     while(slow!=fast) // wanna know cycle entrance point
+     while(slow!=fast)
      {
       slow=nums[slow];
       fast=nums[fast];
      }
-        return slow;
+     return slow;*/
+     for(int i=0;i<n;i++)
+     {
+      if(nums[abs(nums[i])-1]>0)
+          nums[abs(nums[i])-1]*=-1;
+      else
+          ans=abs(nums[i]);
+          
+     }
+     return ans;
+        
     }
+    
+    
+};
 
 // 26. Remove Duplicates from Sorted Array
 
