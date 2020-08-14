0737a062856d064fa1915d756e51aeeed0353d7d
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-15 00:38:15 +0530

    Update 4. Median of Two Sorted Arrays.cpp

--- 4. Median of Two Sorted Arrays.cpp
+++ 4. Median of Two Sorted Arrays.cpp
@@ -1,17 +1,20 @@
  // 4. Median of Two Sorted Arrays
 
 
-double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)//O(m+n)
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) //O(m+n)
     {
-     int m=nums1.size(),n=nums2.size();
-      int cnt=0,i=0,j=0;
-      double ans,ans1;
+     int m=nums1.size();
+     int n=nums2.size();
+     int i=0,j=0,cnt=0;
+     double ans=0,ans1=0;
      if((m+n) & 1)
      {
       while(cnt<=(m+n)/2)
       {
        if(i!=m && j!=n)
-           ans=(nums1[i]>nums2[j]) ? nums2[j++]:nums1[i++];
+           ans=(nums1[i]>nums2[j]) ? nums2[j++] : nums1[i++];
        else if(i<m)
            ans=nums1[i++];
        else
@@ -20,25 +23,25 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)//O(m+n)
       }
        return ans;
      }
-
      else
      {
       while(cnt<=(m+n)/2)
       {
-       ans1=ans;
-       if(i!=m && j!=n)
-           ans=(nums1[i]>nums2[j]) ? nums2[j++]:nums1[i++];
-       else if(i<m)
-           ans=nums1[i++];
-       else
-           ans=nums2[j++];
-       cnt++;
+      ans1=ans;
+      if(i!=m && j!=n)
+          ans=(nums1[i]>nums2[j]) ? nums2[j++] : nums1[i++];
+      else if(i<m)
+          ans=nums1[i++];
+      else
+          ans=nums2[j++];
+      cnt++;
       }
-      return (ans+ans1)/2;
+      return (ans+ans1)/2.0;
+          
      }
-
-
+     
     }
+};
 
 /////////////
 
