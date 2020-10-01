93cfa9f36c32ef1f418de8710dc8cf7a72bf7b91
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-02 00:33:26 +0530

    Update 3. Longest Substring Without Repeating Characters.cpp

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,3 +1,5 @@
+// 3. Longest Substring Without Repeating Characters
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s)
@@ -26,5 +28,32 @@ public:
 
      }
      return max(maxx,i-start);
+    }
+};
+
+// 395. Longest Substring with At Least K Repeating Characters
+
+class Solution {
+public:
+    int longestSubstring(string s, int k) //O(n*log(n))
+    {
+     int n=s.length();
+     if(n==0 || k>n)
+         return 0;
+     if(k==0)
+         return n;
+     int cnt[26]={0};
+     for(int i=0;i<n;i++)
+         cnt[s[i]-'a']++;
+     int idx=0;
+     while(idx<n && cnt[s[idx]-'a']>=k)
+         idx++;
+     if(idx==n)
+         return n;
+     int left=longestSubstring(s.substr(0,idx),k);
+     int right=longestSubstring(s.substr(idx+1),k);
+
+    return max(left,right);
+
     }
 };
