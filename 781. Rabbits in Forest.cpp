a3e7be726e01a936d01a6e76f37ed6539ce45c33
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-11-15 17:40:31 +0530

    Create 781. Rabbits in Forest.cpp

---
+++ 781. Rabbits in Forest.cpp
@@ -0,0 +1,37 @@
+// 781. Rabbits in Forest
+
+/*
+In a forest, each rabbit has some color. Some subset of rabbits (possibly all of them) tell you how many other rabbits have the same color as them. 
+Those answers are placed in an array. Return the minimum number of rabbits that could be in the forest.
+*/
+
+/*
+For example [2,2,2] and [2,2] has the same result (i.e) 3 but [2,2,2,2] should be seperated into two groups and the result will be 6          
+
+-->[1,1]=2
+-->[2,2,2]=3
+-->[3,3,3,3]=4 , but in this -i.e-->answer will be [3,3,3,3,3]=8
+-->[4,4,4,4,4]=5
+-->[5,5,5,5,5,5]=6
+*/
+
+
+
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) 
+    {
+     int n=answers.size();
+     if(n==0)
+         return 0;
+     if(n==1)
+         return 1+answers[0];
+     unordered_map<int,int>mp;
+     for(int i=0;i<n;i++)
+         mp[answers[i]]++;
+     int ans=0;
+     for(auto xx:mp)
+         ans+=((xx.second+xx.first)/(xx.first+1))*(xx.first+1);
+     return ans;
+   }
+};
