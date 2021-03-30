60f8bffe436df1279c385e445f60a84c2b03ee86
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-03-30 13:42:29 +0530

    Update 3. Longest Substring Without Repeating Characters.cpp

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -2,32 +2,56 @@
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s)
+    int lengthOfLongestSubstring(string s) 
     {
-     int n=s.length();
-     if(n==0)return 0;
-     unordered_map<char,int>mp;
-     int maxx=1;
-     int start=0,i=0;
-     while(i<n)
-     {
-      mp[s[i]]++;
-
-      if(mp[s[i]]>1)
+      int n=s.length();
+      int start=0,ans=0;
+      unordered_map<char,int>mp;
+      for(int i=0;i<n;i++)
       {
-       maxx=max(maxx,i-start);
-       while(s[start]!=s[i])
+       mp[s[i]]++;
+       if(mp[s[i]]>1)
        {
-        mp[s[start]]--;
-        start++;
+         ans=max(ans,i-start);
+         while(s[start]!=s[i])
+             mp[s[start]]--,start++;
+         mp[s[start]]--;
+         start++;
+         
        }
-       mp[s[start]]--;
-       start++;
-       }
-      i++;
+      
+      }
+      return max(ans,n-start);
+    }
+};
 
+// 1695. Maximum Erasure Value
+
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums)
+    {
+     int n=nums.size();
+     int pre[n+1];
+     pre[0]=0;
+     for(int i=1;i<=n;i++)
+         pre[i]=pre[i-1]+nums[i-1];
+     unordered_map<int,int>mp;
+     int start=1,ans=0;
+     for(int i=0;i<n;i++)
+     {
+      mp[nums[i]]++;
+      if(mp[nums[i]]>1)
+      {
+       ans=max(ans,pre[i]-pre[start-1]);
+       while(nums[start-1]!=nums[i])
+           mp[nums[start-1]]--,start++;
+       mp[nums[start-1]]--;
+       start++;
+      }
      }
-     return max(maxx,i-start);
+     return max(ans,pre[n]-pre[start-1]);
+     
     }
 };
 
