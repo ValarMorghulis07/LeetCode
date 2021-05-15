628ec7bd4452d14e2eda83b63b80c29085456d56
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-05-15 12:47:37 +0530

    Update Duplicates,Missing,Disappeared.cpp

--- Duplicates,Missing,Disappeared.cpp
+++ Duplicates,Missing,Disappeared.cpp
@@ -230,4 +230,25 @@ for (i = 0; i < size; i++) {
     for (i = 0; i < size; i++) { 
         if (arr[i] > 0) 
             cout << (i + 1); 
+        
+        
+  // 442. Find All Duplicates in an Array
+        
+   class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) 
+    {
+      int n=nums.size();
+      vector<int>ans;
+      for(int i=0;i<n;i++)
+      {
+        if(nums[abs(nums[i])-1]>0)
+            nums[abs(nums[i])-1]*=-1;
+        else
+            ans.push_back(abs(nums[i]));
+      }
+      return ans;
+        
+    }
+};
 
